0a48ba2b537a7d1c96254a221797fbe9c55c8005
praful2345 <112775889+praful2345@users.noreply.github.com>
2023-12-03 11:31:28 +0530

    C:\Praful\Personal123\Python\01_Hello.py

--- 01_Hello.py
+++ 01_Hello.py
@@ -4,7 +4,4 @@ Desc: To display hello world - first prog'''
 import os
 
 print('''hello world how r u
-      Twinkle twinkle little Star 
-      how i wonder what u Are 
-      up above the work so high 
-      like a diamond in the sky''')
+      Twin This is a chnge''')
